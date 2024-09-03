ff19772f44ff0439c44ea2a0e00e952ba6b7f7d2
Sebastian Rosnes Sørensen <133763422+SebastianRSorensen@users.noreply.github.com>
2024-09-03 18:24:49 +0200

    First commit

---
+++ src/test/java/FirstJavaSpring/HVL/HvlSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package FirstJavaSpring.HVL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvlSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
